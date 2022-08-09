31a8a3cb59e941451ddb9f65001294a9f2ad2369
baogg <b_guog@163.com>
2022-08-09 11:33:44 +0800

    git push for idea

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,9 +3,9 @@
  */
 public class GitTest {
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git3333!");
+        System.out.println("push test");
     }
 }
